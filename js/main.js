3929774fcf23ab2e2c960c258b560da269ca4491
Constantin Pojoga <pojogac@gmail.com>
2016-08-29 14:35:37 -0500

    deploy

--- js/main.js
+++ js/main.js
@@ -1,11 +1,3 @@
-// $('#nav').affix({
-//       offset: {
-//         top: $('section:nth-child(1)').outerHeight()
-//       }
-// }); 
-
-console.log($('section:nth-child(1)').outerHeight());
-
 
 $('.navbar li:nth-child(2) a').click(function(event) {
     event.preventDefault();
@@ -18,3 +10,4 @@ $('.navbar li:nth-child(3) a').click(function(event) {
     $($(this).attr('href'))[0].scrollIntoView();
     scrollBy(0, -50);
 });
+
