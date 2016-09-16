9c8312467b4d3a6434602845a5cb8dbd317753e7
Constantin Pojoga <pojogac@gmail.com>
2016-09-16 17:38:57 -0500

    project preview animation, add down-arrow.gif

--- js/main.js
+++ js/main.js
@@ -11,3 +11,22 @@ $('.navbar li:nth-child(3) a').click(function(event) {
     scrollBy(0, -50);
 });
 
+$( document ).ready(function() {
+    $("[rel='tooltip']").tooltip();    
+    $('.thumbnail').hover(
+        function(){
+          if ($(window).width() >=768) {
+            $(this).find('.caption')
+            .slideDown(250);
+             // .fadeIn(400) 
+          }    
+        },
+        function(){
+            $(this).find('.caption')
+            .slideUp(250); 
+            // .fadeOut(500)
+        }
+    ); 
+});
+
+
